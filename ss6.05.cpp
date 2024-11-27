1558aa7c078f8148a05c92f289abe02430c8ed56
hoang1811-coder <vhoangssz1@gmail.com>
2024-11-27 12:15:14 +0700

    Upload

---
+++ ss6.05.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int main() {
+    int year, month, days;
+    printf("Nhap nam: ");
+    scanf("%d", &year);
+    printf("Nhap thang: ");
+    scanf("%d", &month);
+
+    if (month < 1 || month > 12) {
+        printf("Thang khong hop le! Vui long nhap thang tu 1 den 12.\n");
+    } else {
+        switch (month) {
+            case 1:  // Tháng 1
+            case 3:  // Tháng 3
+            case 5:  // Tháng 5
+            case 7:  // Tháng 7
+            case 8:  // Tháng 8
+            case 10: // Tháng 10
+            case 12: // Tháng 12
+                days = 31;
+                break;
+            case 4:  // Tháng 4
+            case 6:  // Tháng 6
+            case 9:  // Tháng 9
+            case 11: // Tháng 11
+                days = 30;
+                break;
+            case 2:  // Tháng 2
+
+                if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+                    days = 29;  
+                } else {
+                    days = 28;  
+                }
+                break;
+            default:
+                printf("Thang khong hop le! Vui long nhap thang tu 1 den 12.\n");
+                return 1;
+        }
+
+        // In k?t qu?
+        printf("Thang %d, nam %d co %d ngay.\n", month, year, days);
+    }
+
+    return 0;
+}
